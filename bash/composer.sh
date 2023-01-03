6624ba24bfe613cae27fa18914393818d098f8d6
Phil Chekhov <me@aicantar.com>
2023-01-03 20:46:44 +0600

    Major `composer.sh` refactoring

--- bash/composer.sh
+++ bash/composer.sh
@@ -1,82 +1,118 @@
 #!/usr/bin/env bash
 
+# Print usage information.
 usage() {
     cat << USAGE
-Usage: composer [CONTAINER-OPTIONS] [-- [COMPOSER-OPTIONS]...] [COMPOSER-COMMAND]
+Usage: composer [CONTAINER-OPTIONS] COMPOSER-COMMAND
+       composer [CONTAINER-OPTIONS] [-- COMPOSER_OPTIONS...] COMPOSER-COMMAND
 
 Container options:
-  -i <version>      Composer image version, default is 2
-  -d <directory>    Composer home directory, default is \$HOME/.tools-in-containers/composer/home
-  -c <directory>    Composer cache directory, default is \$HOME/.tools-in-containers/composer/cache
-  -w <directory>    Location to mount into the container as working directory, default is the current working directory
-  -h                Show this information and exit
+      --image-tag   Composer image tag, default is 2
+      --home-dir    Location to mount as Composer home directory, default is
+                    \$HOME/.tools-in-containers/composer/home
+      --cache-dir   Location to mount as Composer cache directory, default is
+                    \$HOME/.tools-in-containers/composer/cache
+  -w, --working-dir Location to mount as working directory, default is the
+                    current working directory
+  -h, --help        Show this information and exit
+
+If you need to pass any Composer option that starts with -- (e.g. --no-cache),
+separate it from the container options with --, i.e.:
+
+  composer -- --no-cache
+
+This is due to the behavior of getopt(1) which is used by this script to parse
+the command line arguments.
 
 More info at https://github.com/aicantar/tools-in-containers.
 USAGE
 }
 
-# TODO: come up with a better way to check whether podman or docker are installed.
+# Locate either Docker or Podman executable and return its path.
+get_runner() {
+    # TODO: any better way?
+    runner=$(command -v docker)
 
-_runner=$(command -v docker)
+    if [ ! -x "$runner" ]; then
+        runner=$(command -v podman)
 
-if [ ! -x "$_runner" ]; then
-    _runner=$(command -v podman)
+        if [ ! -x "$runner" ]; then
+            exit 1
+        fi
+    fi
 
-    if [ ! -x "$_runner" ]; then
-        echo "You must have either podman or docker installed. Can't detect either, exiting."
+    echo "$runner"
+}
+
+# Parse command line options.
+parse_opts() {
+    short_opts="hw:"
+    long_opts="help,working-dir:,image-tag:,home-dir:,cache-dir:"
+    parsed_opts=$(getopt -n composer -o "$short_opts" -l "$long_opts" -- "$@")
+    opts_end=0
+
+    composer_image_tag=2
+    composer_home_dir="$HOME/.tools-in-containers/composer/home"
+    composer_cache_dir="$HOME/.tools-in-containers/composer/cache"
+    composer_working_dir=$(pwd)
+    composer_args=( )
+
+    if [ $? -ne 0 ]; then
         exit 1
     fi
-fi
 
-_opts="hi:d:c:w:"
-_opts_parsed=$(getopt -n composer -o $_opts -- "$@")
+    for opt in $parsed_opts; do
+        if [ $opts_end -eq 1 ]; then
+            composer_args+=( "$opt" )
+            continue
+        fi
 
-if [ $? != 0 ]; then
-    usage
-    exit 1
-fi
+        case $opt in
+            -h | --help)        usage;                       exit 1 ;;
+            -w | --working-dir) composer_working_dir=$2;    shift 2 ;;
+            --image-tag)        composer_image_tag=$2;      shift 2 ;;
+            --home-dir)         composer_home_dir=$2;       shift 2 ;;
+            --cache-dir)        composer_cache_dir=$2;      shift 2 ;;
+            --)                 opts_end=1                          ;;
+        esac
+    done
 
-_opts_end=0
-_composer_version=2
-_composer_home_dir="$HOME/.tools-in-containers/composer/home"
-_composer_cache_dir="$HOME/.tools-in-containers/composer/cache"
-_composer_working_directory=$(pwd)
-_composer_args=( )
+    # TODO: any better way?
+    export composer_image_tag
+    export composer_home_dir
+    export composer_cache_dir
+    export composer_working_dir
+    export composer_args
+}
 
-for opt in $_opts_parsed; do
-    if [ $_opts_end -eq 1 ]; then
-        _composer_args+=( "$opt" )
-    fi
+main() {
+    runner=$(get_runner)
 
-    if [ "$opt" == "-h" ]; then
+    if [ $? -ne 0 ]; then
         usage
-        exit
-    elif [ "$opt" == "-i" ]; then
-        _composer_version=$2
-        shift 2
-    elif [ "$opt" == "-d" ]; then
-        _composer_home_dir=$2
-        shift 2
-    elif [ "$opt" == "-c" ]; then
-        _composer_cache_dir=$2
-        shift 2
-    elif [ "$opt" == "-w" ]; then
-        _composer_working_directory=$2
-        shift 2
-    elif [ "$opt" == "--" ]; then
-        _opts_end=1
+        exit 1
     fi
-done
 
-mkdir -p "$_composer_home_dir" "$_composer_cache_dir"
+    parse_opts "$@"
 
-$_runner run                                                \
-    --rm                                                    \
-    --interactive                                           \
-    --tty                                                   \
-    --env "COMPOSER_HOME_DIR=/composer-home"                \
-    --env "COMPOSER_CACHE_DIR=/composer-cache"              \
-    --volume "$_composer_home_dir:/composer-home:rw,z"      \
-    --volume "$_composer_cache_dir:/composer-cache:rw,z"    \
-    --volume "$_composer_working_directory:/app:rw,z"       \
-    docker.io/composer:"$_composer_version" composer "${_composer_args[@]//\'/}"
+    if [ $? -ne 0 ]; then
+        usage
+        exit 1
+    fi
+
+    mkdir -p "$composer_home_dir" "$composer_cache_dir"
+
+    $runner run                                                 \
+        --rm                                                    \
+        --interactive                                           \
+        --tty                                                   \
+        --env "COMPOSER_HOME_DIR=/composer-home"                \
+        --env "COMPOSER_CACHE_DIR=/composer-cache"              \
+        --volume "$composer_home_dir:/composer-home:rw,z"       \
+        --volume "$composer_cache_dir:/composer-cache:rw,z"     \
+        --volume "$composer_working_dir:/app:rw,z"              \
+        docker.io/composer:"$composer_image_tag" composer "${composer_args[@]//\'/}"
+
+}
+
+main "$@"
