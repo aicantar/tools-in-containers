2e5510d660297b227c4e359ff6ee2390e066e79c
Phil Chekhov <me@aicantar.com>
2023-01-04 12:29:42 +0600

    `composer.sh` refactoring

--- bash/composer.sh
+++ bash/composer.sh
@@ -12,7 +12,7 @@ Container options:
                     \$HOME/.tools-in-containers/composer/home
       --cache-dir   Location to mount as Composer cache directory, default is
                     \$HOME/.tools-in-containers/composer/cache
-  -w, --working-dir Location to mount as working directory, default is the
+  -w, --workdir     Location to mount as working directory, default is the
                     current working directory
   -h, --help        Show this information and exit
 
@@ -44,44 +44,41 @@ get_runner() {
     echo "$runner"
 }
 
-# Parse command line options.
-parse_opts() {
-    short_opts="hw:"
-    long_opts="help,working-dir:,image-tag:,home-dir:,cache-dir:"
-    parsed_opts=$(getopt -n composer -o "$short_opts" -l "$long_opts" -- "$@")
-    opts_end=0
+# Parse command line arguments.
+parse_options() {
+    short_options="hw:"
+    long_options="help,workdir:,image-tag:,home-dir:,cache-dir:"
+    parsed_options=$(getopt -n composer -o "$short_options" -l "$long_options" -- "$@")
+    options_end=0
+
+    [ $? -ne 0 ] && exit 1
 
     composer_image_tag=2
     composer_home_dir="$HOME/.tools-in-containers/composer/home"
     composer_cache_dir="$HOME/.tools-in-containers/composer/cache"
-    composer_working_dir=$(pwd)
+    composer_workdir=$(pwd)
     composer_args=( )
 
-    if [ $? -ne 0 ]; then
-        exit 1
-    fi
-
-    for opt in $parsed_opts; do
-        if [ $opts_end -eq 1 ]; then
-            composer_args+=( "$opt" )
+    for option in $parsed_options; do
+        if [ $options_end -eq 1 ]; then
+            composer_args+=( "$option" )
             continue
         fi
 
-        case $opt in
+        case "$option" in
             -h | --help)        usage;                       exit 1 ;;
-            -w | --working-dir) composer_working_dir=$2;    shift 2 ;;
+            -w | --workdir)     composer_workdir=$2;        shift 2 ;;
             --image-tag)        composer_image_tag=$2;      shift 2 ;;
             --home-dir)         composer_home_dir=$2;       shift 2 ;;
             --cache-dir)        composer_cache_dir=$2;      shift 2 ;;
-            --)                 opts_end=1                          ;;
+            --)                 options_end=1                       ;;
         esac
     done
 
-    # TODO: any better way?
     export composer_image_tag
     export composer_home_dir
     export composer_cache_dir
-    export composer_working_dir
+    export composer_workdir
     export composer_args
 }
 
@@ -93,7 +90,7 @@ main() {
         exit 1
     fi
 
-    parse_opts "$@"
+    parse_options "$@"
 
     if [ $? -ne 0 ]; then
         usage
@@ -110,9 +107,8 @@ main() {
         --env "COMPOSER_CACHE_DIR=/composer-cache"              \
         --volume "$composer_home_dir:/composer-home:rw,z"       \
         --volume "$composer_cache_dir:/composer-cache:rw,z"     \
-        --volume "$composer_working_dir:/app:rw,z"              \
+        --volume "$composer_workdir:/app:rw,z"              \
         docker.io/composer:"$composer_image_tag" composer "${composer_args[@]//\'/}"
-
 }
 
 main "$@"
